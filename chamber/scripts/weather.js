4ade9a94307918620c1683b6d40adbd313d9c441
guifabri <erickscala_19@hotmail.com>
2024-11-27 10:04:47 -0400

    changes

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,67 @@
+// https://openweathermap.org/current
+// https://openweathermap.org/weathermap
+// API key: 0aec6412eede92415f476e7030db63f4
+const apiKey = "0aec6412eede92415f476e7030db63f4";
+// AVONDALE ARIZONA
+// 33.44450069074283, -112.35577444568536
+// Litchfield Park Arizona
+// 33.49531346879897, -112.35600589274492
+
+const lat = 33.495313;
+const lon = -112.356;
+
+const url = https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey};
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);
+            displayResults(data);
+        } else {
+            throw new Error(await response.text());
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+apiFetch();
+
+function displayResults(data) {
+    const location = document.querySelector("#location");
+
+    const tempArea = document.querySelector(".temp-area");
+
+    const captionDesc = document.querySelector("#weather-description");
+    const feelsLike = document.querySelector("#feels-like");
+    const windSpeed = document.querySelector("#wind-speed");
+    const humidity = document.querySelector("#humidity");
+
+    const weatherIcon = document.createElement("img");
+    tempArea.appendChild(weatherIcon);
+
+    const currentTemp = document.createElement("span");
+    currentTemp.setAttribute("id", "current-temp");
+    tempArea.appendChild(currentTemp);
+
+    location.innerHTML = data.name;
+    // Format temperature to show zero decimal points
+    const formattedTemp = data.main.temp.toFixed(0);
+    // Display current temperature
+    currentTemp.innerHTML = ${formattedTemp}&deg;F;
+    feelsLike.innerHTML = ${data.main.feels_like.toFixed(0)}&deg;F;
+    windSpeed.innerHTML = ${data.wind.speed.toFixed(0)}mph;
+    humidity.innerHTML = ${data.main.humidity}%;
+
+    // Display weather icon and description
+    data.weather.forEach((weatherEvent) => {
+        const iconsrc = https://openweathermap.org/img/wn/${weatherEvent.icon}@2x.png;
+        let desc = weatherEvent.description;
+        weatherIcon.setAttribute("src", iconsrc);
+        weatherIcon.setAttribute("alt", desc);
+        weatherIcon.setAttribute("width", "100");
+        weatherIcon.setAttribute("height", "100");
+        captionDesc.innerHTML = ${desc};
+    });
+}

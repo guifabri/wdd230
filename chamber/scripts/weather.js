ff6523e6eb53cb8102a2ad88d8756381de960333
guifabri <erickscala_19@hotmail.com>
2024-11-27 21:19:01 -0400

    changes

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -7,10 +7,10 @@ const apiKey = "0aec6412eede92415f476e7030db63f4";
 // Litchfield Park Arizona
 // 33.49531346879897, -112.35600589274492
 
-const lat = 33.495313;
-const lon = -112.356;
+const lat = 7.813558;
+const lon = -72.444585;
 
-const url = https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey};
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
 async function apiFetch() {
     try {
@@ -49,19 +49,19 @@ function displayResults(data) {
     // Format temperature to show zero decimal points
     const formattedTemp = data.main.temp.toFixed(0);
     // Display current temperature
-    currentTemp.innerHTML = ${formattedTemp}&deg;F;
-    feelsLike.innerHTML = ${data.main.feels_like.toFixed(0)}&deg;F;
-    windSpeed.innerHTML = ${data.wind.speed.toFixed(0)}mph;
-    humidity.innerHTML = ${data.main.humidity}%;
+    currentTemp.innerHTML = `${formattedTemp}&deg;F`;
+    feelsLike.innerHTML = `${data.main.feels_like.toFixed(0)}&deg;F`;
+    windSpeed.innerHTML = `${data.wind.speed.toFixed(0)}mph`;
+    humidity.innerHTML = `${data.main.humidity}%`;
 
     // Display weather icon and description
     data.weather.forEach((weatherEvent) => {
-        const iconsrc = https://openweathermap.org/img/wn/${weatherEvent.icon}@2x.png;
+        const iconsrc = `https://openweathermap.org/img/wn/${weatherEvent.icon}@2x.png`;
         let desc = weatherEvent.description;
         weatherIcon.setAttribute("src", iconsrc);
         weatherIcon.setAttribute("alt", desc);
         weatherIcon.setAttribute("width", "100");
         weatherIcon.setAttribute("height", "100");
-        captionDesc.innerHTML = ${desc};
+        captionDesc.innerHTML = `${desc}`;
     });
 }

11bb739300cff3f666f29e8a8ba7ac6e5d9ba011
guifabri <erickscala_19@hotmail.com>
2024-12-13 22:36:02 -0400

    changes

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,26 @@
 const baseURL = "https://guifabri.github.io/wdd230/";
 const linksURL = "https://guifabri.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  displayLinks(data);
+}
+
+function displayLinks(data) {
+  const linksContainer = document.querySelector("ul.links"); // Selecciona el ul con la clase "links"
+
+  data.weeks.forEach((week) => {
+    // Para cada semana, recorre sus enlaces
+    week.links.forEach((link) => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      a.href = `${baseURL}${link.url}`; // Construye la URL completa
+      a.textContent = link.title; // Título del enlace
+      li.appendChild(a);
+      linksContainer.appendChild(li); // Agrega el <li> al contenedor
+    });
+  });
+}
+
+getLinks();
